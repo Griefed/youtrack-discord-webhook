1c5bf60f565ced4a77ca0c43e634932a914615a9
Griefed <griefed@griefed.de>
2023-04-28 21:31:51 +0200

    Add onChange notification
    Triggers when the change of the issue does not match becomesReported, becomesResolved or becomesUnresolved

--- main.js
+++ main.js
@@ -5,41 +5,6 @@ const CONFIG = require("./config");
 const {Payload} = require("./payload");
 const {Embed, Body, Author, Field, Footer} = require("./embed");
 
-const EVENTS = [
-    {
-        title: "Stage Changed",
-        newDescription: "Stage set to $newValue.",
-        changeDescription: "Stage changed from $oldValue to $newValue.",
-        issueKey: "Stage",
-        nameKey: "name"
-    },
-    {
-        title: "Assignee Changed",
-        newDescription: "Assignee set to $newValue.",
-        changeDescription: "Assignee changed from $oldValue to $newValue.",
-        issueKey: "Assignee",
-        nameKey: "visibleName"
-    },
-    {
-        title: "Comment Added",
-        newDescription: "$newValue",
-        customCheck: function(issue) {
-            return issue.comments.isChanged;
-        },
-        valueGetter: function(issue) {
-            if (issue.comments.added.size < 1) return "lmao i can't code";
-            return issue.comments.added.get(0).text;
-        }
-    },
-    {
-        title: "Priority Changed",
-        newDescription: "The issue priority was set to $newValue.",
-        changeDescription: "The issue priority was changed from $oldValue to $newValue.",
-        issueKey: "Priority",
-        nameKey: "name"
-    }
-];
-
 exports.rule = entities.Issue.onChange({
     title: "Discord Webhook",
     guard: (ctx) => {
@@ -92,56 +57,37 @@ exports.rule = entities.Issue.onChange({
 
             return;
         }
-        
-        let changes = [];
-        for (let i = 0; i < EVENTS.length; i++) {
-            const event = EVENTS[i];
-            const issueKey = event.issueKey;
-            if (!((issueKey && issue.fields[issueKey] && issue.isChanged(issueKey)) || (event.customCheck && event.customCheck(issue)))) continue;
+        else if (issue.becomesUnresolved) {
+            const payload = new Payload(null, CONFIG.SENDER_NAME, CONFIG.AVATAR_URL);
+            const embed = new Embed();
+            const body = new Body();
 
-            let oldValue;
-            let newValue;
+            body.title = "Issue " + issue.id + " Unresolved";
+            body.description = issue.description;
+            body.url = issue.url;
+            body.color = CONFIG.COLOR_NEGATIVE;
+            body.setDateToNow();
+            embed.body = body;
 
-            if (issueKey) {
-                oldValue = issue.oldValue(issueKey);
-                newValue = issue.fields[issueKey];
-            }
+            const user = ctx.currentUser;
+            embed.author = new Author(user.visibleName, CONFIG.YOUTRACK_URL + "/users/" + user.login);
 
-            if (event.valueGetter) newValue = event.valueGetter(issue);
+            embed.footer = new Footer(CONFIG.SITE_NAME + " " + issue.project.name);
 
-            if (event.nameKey) {
-                if (oldValue) oldValue = oldValue[event.nameKey];
-                newValue = newValue[event.nameKey];
-            }
+            payload.addEmbed(embed);
+            payload.send(CONFIG.WEBHOOK_URL);
 
-            let description = oldValue ? event.changeDescription : event.newDescription;
-
-            changes.push({
-                title: event.title,
-                description: description.replace("$oldValue", oldValue).replace("$newValue", newValue)
-            });
+            return;
         }
-
-        const changeCount = changes.length;
-        if (changeCount < 1) return;
-
-        const payload = new Payload(null, CONFIG.SENDER_NAME, CONFIG.AVATAR_URL);
+        
+    	const payload = new Payload(null, CONFIG.SENDER_NAME, CONFIG.AVATAR_URL);
         const embed = new Embed();
-
         const body = new Body();
-        
-        if (changeCount === 1) {
-            const change = changes[0];
-            body.title = change.title + " In " + issue.id;
-            body.description = change.description;
-        }
-        else {
-            body.title = changeCount + " New Changes To " + issue.id;
-            for (let i = 0; i < changes.length; i++) embed.addField(new Field(changes[i].title, changes[i].description, false));
-        }
 
+        body.title = "Issue " + issue.id + " " + issue.fields.State.name;
+        body.description = issue.description;
         body.url = issue.url;
-        body.color = CONFIG.COLOR_REGULAR;
+        body.color = CONFIG.COLOR_CHANGED;
         body.setDateToNow();
         embed.body = body;
 
